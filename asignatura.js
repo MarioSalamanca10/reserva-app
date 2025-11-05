98b8f86d4d647ba3a69bbf155bbadcb1a162a284
MarioSalamanca10 <jomasago10@gmail.com>
2025-11-05 07:34:12 -0500

    asignaturas - docentes espacios vacios

--- asignatura.js
+++ asignatura.js
@@ -1,16 +1,16 @@
 const API_URL = "https://reserva-app-1.onrender.com";
 
 const profesoresPorMateria = {
-  "Matematicas": [" Yessica Daniela Bermudez Vargas"," Carlos Augusto Castro Mendoza"," Andrea Fajardo Jaman"," Anna Lamonova "," Yazmin Guzmán Bejarano"," Juan David López Baquero"," Victor Alfonso Lopez Cordoba"," Andrea Catalina López Zuluaga"," Brahyam Ricardo Maldonado Ibagué"," Karen Maria Newball Benitez"],
-  "Ciencias": [" Jossy Esteban Clavijo Aguirre"," Juan Sebastian González Gómez"," Camilo Andres Martínez Quijano"," Oscar Felipe Moreno Gómez"," Santiago Velásquez Murcia"],
-  "Lenguaje": [" Julieth Alexandra Ayala Venegas"," Diego Alberto Caballero Franco"," Anjuly Raysa Gomez Gomez"," Jessica Catherine Lozano Cañón"," Madelein Quevedo Rodríguez"," Laura Paola Tamayo Munar"," Laura Camila Hernández Chacón"],
-  "Sociales": [""],
-  "Ingles": [" Daniel Fabian Bernal Quintanilla"," Angie Katherine Pacheco Moreno"," Ana Belén Villamil Murcia"],
-  "Educacion_Fisica": [" Diego Eduardo Botia Garzon"," Luis Alejandro Castro Urrego"," Oscar David Cerinza Pinzon"," Juan David Montejo Rodríguez"," Daniel Elías  Rodríguez Cabarcas"," Diego Alejandro Romero Satizabal"," Luis Fernando Torres Rodríguez"," Juan Sebastian Vasquez Gómez"],
-  "Psicologia": [" Joyce Cuéllar Reyes"," Yuly Constanza Murillo Fajardo"," Diana Melissa Niño Ramírez"," Claudia Margarita Rivera Espinosa"," Leonardo Rivera González"],
-  "Tecnologia": [" Jhon Fredt Posada Posada"],
-  "Artes": [" Daniel Oswaldo Cordon"," Edward Fernando Daza Rivera"," Laura Brigith Gil Duran"," Erika Marcela Herrera Castillo"," Angélica Sofia Lagos Tovar"," Cindi Alejandra Lopez Cepeda"," Monica Yazmin Vargas Juyar"," Karen Sofia Guevara Carreño"," Deyby Steven Salamanca Cartagena"," Diego Mauricio Salgado Parrado"," Luis Carlos Valero Moreno"],
-  "Frances": [" Laura Patricia Avila Montenegro"]
+  "Matematicas": [" "," Yessica Daniela Bermudez Vargas"," Carlos Augusto Castro Mendoza"," Andrea Fajardo Jaman"," Anna Lamonova "," Yazmin Guzmán Bejarano"," Juan David López Baquero"," Victor Alfonso Lopez Cordoba"," Andrea Catalina López Zuluaga"," Brahyam Ricardo Maldonado Ibagué"," Karen Maria Newball Benitez"],
+  "Ciencias": [" ", " Jossy Esteban Clavijo Aguirre"," Juan Sebastian González Gómez"," Camilo Andres Martínez Quijano"," Oscar Felipe Moreno Gómez"," Santiago Velásquez Murcia"],
+  "Lenguaje": [" ", " Julieth Alexandra Ayala Venegas"," Diego Alberto Caballero Franco"," Anjuly Raysa Gomez Gomez"," Jessica Catherine Lozano Cañón"," Madelein Quevedo Rodríguez"," Laura Paola Tamayo Munar"," Laura Camila Hernández Chacón"],
+  "Sociales": [" "],
+  "Ingles": [" ", " Daniel Fabian Bernal Quintanilla"," Angie Katherine Pacheco Moreno"," Ana Belén Villamil Murcia"],
+  "Educacion_Fisica": [" ", " Diego Eduardo Botia Garzon"," Luis Alejandro Castro Urrego"," Oscar David Cerinza Pinzon"," Juan David Montejo Rodríguez"," Daniel Elías  Rodríguez Cabarcas"," Diego Alejandro Romero Satizabal"," Luis Fernando Torres Rodríguez"," Juan Sebastian Vasquez Gómez"],
+  "Psicologia": [" ", " Joyce Cuéllar Reyes"," Yuly Constanza Murillo Fajardo"," Diana Melissa Niño Ramírez"," Claudia Margarita Rivera Espinosa"," Leonardo Rivera González"],
+  "Tecnologia": [" ", " Jhon Fredt Posada Posada"],
+  "Artes": [" ", " Daniel Oswaldo Cordon"," Edward Fernando Daza Rivera"," Laura Brigith Gil Duran"," Erika Marcela Herrera Castillo"," Angélica Sofia Lagos Tovar"," Cindi Alejandra Lopez Cepeda"," Monica Yazmin Vargas Juyar"," Karen Sofia Guevara Carreño"," Deyby Steven Salamanca Cartagena"," Diego Mauricio Salgado Parrado"," Luis Carlos Valero Moreno"],
+  "Frances": [" ", " Laura Patricia Avila Montenegro"]
 };
 
 const HorasDisponibles = [
